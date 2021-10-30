ef1fa9c037d034cf33d4d1793da94e0454673e66
unknown <manuelcaceres455@gmail.com>
2021-10-30 09:44:32 -0400

    Prep fusion

--- Samancito delivery/src/samancito/delivery/SamancitoDelivery.java
+++ Samancito delivery/src/samancito/delivery/SamancitoDelivery.java
@@ -1,14 +1,14 @@
 
 package samancito.delivery;
 
-import Clases.Base_de_Datos;
+
 
 public class SamancitoDelivery {
 
     
     public static void main(String[] args) {
-        Base_de_Datos a = new Base_de_Datos();
-        a.datos();
+
+        
     }
     
 }
